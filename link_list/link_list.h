5138554ebcd5c7e26aa96734a810e5e3c682a626
sarailQAQ <847117505@qq.com>
2020-09-23 13:04:56 +0800

    first commit

---
+++ link_list/link_list.h
@@ -0,0 +1,114 @@
+//
+// Created by sarail on 2020/9/22.
+//
+
+#ifndef LINKLIST_LINK_LIST_H
+#define LINKLIST_LINK_LIST_H
+#include <cstdio>
+#include <iostream>
+
+// as a habit of Go,the private object is in lowercase letter
+// and the public is in uppercase letter
+template <typename T> class LinkList {
+private:
+    struct node{
+        T data;
+        node* next;
+        node() : next(nullptr) {}
+    }*head = nullptr;
+    int size;
+
+public:
+    LinkList():head(nullptr), size(0) {}
+
+    void Insert(const T& val) {
+        // empty list
+        size++;
+        auto p = new node;
+        p->data = val;
+        if (head == nullptr) {
+            head = p;
+            return;
+        }
+
+        // find the position of val
+        auto n = head;
+        if (n->data > val) {
+            p->next = n;
+            head = p;
+            return;
+        }
+
+        while (n != nullptr) {
+            if (n->next == nullptr || n->next->data > val) break;
+            n = n->next;
+        }
+        p->next = n->next;
+        n->next = p;
+
+        // magic bug
+        // without this return,the void function in class will not return and continue to run
+        return;
+    }
+
+    bool Delete(T val) {
+        if (head == nullptr) return false;
+        if (head->data == val) {
+            head = head->next;
+            size--;
+            return true;
+        }
+
+        auto n = head;
+        while (n->next != nullptr && n->next->data < val) n = n->next;
+        if (n->next == nullptr || n->next->data != val) return false;
+        n->next = n->next->next;
+        size--;
+    }
+
+    T DeleteMin() {
+        if (head == nullptr) {
+            // I have not learned how to through a panic in c++ yet
+            // maybe return NULL would cause an error
+            printf("error:delete on empty link-list");
+            return NULL;
+        }
+
+        // head is the minimum element
+        head = head->next;
+        size--;
+        return head->data;
+    }
+
+    // just reverse all next points and update head
+    void Reverse() {
+        if (size < 2) return;
+        auto pre = head;
+        auto n = pre->next;
+
+        while (n != nullptr) {
+            auto nxt = n->next;
+            n->next = pre;
+            pre = n;
+            n = nxt;
+        }
+        head->next = nullptr;
+        head = pre;
+    }
+
+    void PrintAll() {
+        auto n = head;
+        while (n != nullptr) {
+            std::cout << n->data << " ";
+            n = n->next;
+        }
+        std::cout << "\n";
+        return;
+    }
+
+    int Size() {
+        return size;
+    }
+};
+
+#endif //LINKLIST_LINK_LIST_H
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <ctime>
+#include "link_list/link_list.h"
+using namespace std;
+
+// maintain a ordered list
+void task1() {
+    LinkList<int> L;
+    for (int i = 0; i < 10; ++i) L.Insert(rand() % 100);
+    L.PrintAll();
+    int a = rand() % 100;
+    cout << "insert:" << a << endl;
+    L.Insert(a);
+    L.PrintAll();
+}
+
+// delete the minimum element of the list
+// o(n)
+void task2() {
+    LinkList<int> L;
+    for (int i = 0; i < 10; ++i) L.Insert(rand() % 100);
+    L.PrintAll();
+    L.DeleteMin();
+    L.PrintAll();
+}
+
+// reverse the ordered list
+// o(n)
+void task3() {
+    LinkList<int> L;
+    for (int i = 0; i < 10; ++i) L.Insert(rand() % 100);
+    L.PrintAll();
+    L.Reverse();
+    L.PrintAll();
+}
+
+int main() {
+
+    srand(time(0));
+//    task1();
+//    task2();
+    task3();
+    return 0;
+}
